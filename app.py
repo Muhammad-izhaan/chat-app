b906abe503712453a2973729f4400d3d433b9935
Muhammad-izhaan <161720356+Muhammad-izhaan@users.noreply.github.com>
2024-11-22 23:38:58 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,68 +1,32 @@
-# First, import eventlet and call monkey_patch
-import eventlet
-eventlet.monkey_patch()
-
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, request, session
 from flask_socketio import SocketIO, emit
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-import os
+import secrets
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # Change this to your database URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = secrets.token_hex(16)
+socketio = SocketIO(app)
 
-db = SQLAlchemy(app)
-socketio = SocketIO(app, cors_allowed_origins="*")
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-
-# User model
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
-
-# Load user
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-# Routes
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-        if user and user.password == password:  # Replace this with hashed password check
-            login_user(user)
-            return redirect(url_for('chat'))
-        flash('Login failed. Check your username and password.')
-    return render_template('login.html')
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
 
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
+@socketio.on('join')
+def handle_join(data):
+    username = data['username']
+    session['username'] = username
+    emit('user_joined', {'username': username}, broadcast=True)
 
-@app.route('/chat')
-@login_required
-def chat():
-    return render_template('chat.html')
-
-# SocketIO events
 @socketio.on('message')
-def handle_message(msg):
-    print('Message: ' + msg)
-    emit('response', msg, broadcast=True)
+def handle_message(data):
+    username = session.get('username', 'Anonymous')
+    emit('message', {
+        'username': username,
+        'message': data['message']
+    }, broadcast=True)
 
 if __name__ == '__main__':
-    db.create_all()  # Creates the database tables
-    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    socketio.run(app, debug=True)
